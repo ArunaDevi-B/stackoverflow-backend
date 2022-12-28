65507fdc8e3f9d16961a979f8e16cb03222762fc
ArunaDevi-B <b.arunadevi30@gmail.com>
2022-12-28 10:16:06 +0530

    final changes

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const db = require ('./db.js');
-const PORT = process.env.PORT || 80 
-const router = require("./routers");
-
+// const db = require ('./db.js');
+const PORT = process.env.PORT || 8000 
+// const router = require("./backend/routers");
+const router = require("./backend/routers")
+const db = require("./backend/db")
 //DB Connection
 db.connect();
 
@@ -35,16 +36,16 @@ app.use('/api', router)
 
 //static resources
 
-app.use('/upload', express.static(path.join(__dirname, '/../uploads')))
-app.use(express.static(path.join(__dirname, '/../frontend/build')))
+// app.use('/upload', express.static(path.join(__dirname, '/../uploads')))
+// app.use(express.static(path.join(__dirname, '/../frontend/build')))
 
-app.get('*', (req,res) =>{
-    try{
-        res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`))
-    }catch{
-        res.send('Oops! error occured')
-    }
-})
+// app.get('*', (req,res) =>{
+//     try{
+//         res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`))
+//     }catch{
+//         res.send('Oops! error occured')
+//     }
+// })
 
 // cors 
 
